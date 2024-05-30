032ac5f7d1ce6b093cca203bf941c6a44ad68f6b
Dev-Jathu <jathusankunam005@mail.com>
2024-05-30 15:24:52 +0530

    push for master

--- Client/src/Pages/Signup/Signupfarmer/Signup.js
+++ Client/src/Pages/Signup/Signupfarmer/Signup.js
@@ -329,7 +329,7 @@ function Signup() {
 
     const userdata = { Name, NIC, TelNo, Email, Password };
     try {
-      const response = await fetch("http://localhost:3003/farmer/register", {
+      const response = await fetch("https://uzhavan-server.onrender.com/farmer/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
