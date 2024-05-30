e6c0b1786d6b850f5c1e65ac5b78226a885e3222
Dev-Jathu <jathusankunam005@mail.com>
2024-05-30 20:30:14 +0530

    change url

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://uzhavan-dev-jathus-projects.vercel.app"],
+    origin: ["https://uzhavan-0mr6.onrender.com"],
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials: true,
   })
